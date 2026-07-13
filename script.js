8dd3db76e9acdd93ee03257be9bee1bbbde71560
pipipapipo <88481207+pipipapipo@users.noreply.github.com>
2026-07-13 23:02:39 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-if (menuButton && navLinks) {
-  menuButton.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-  });
-}
